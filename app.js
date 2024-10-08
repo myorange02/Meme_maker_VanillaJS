7974b1275354685ae2ebbad3e041da45c53da566
myorange02 <94654972+myorange02@users.noreply.github.com>
2024-10-08 08:16:31 +0000

    #2.4 Paint Color pt.2

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const canvas = document.querySelector("canvas");
 const lineWidth = document.getElementById("line-width");
 const color = document.getElementById("color");
+const colorOptions = Array.from(document.getElementsByClassName("color-option"));
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
@@ -35,6 +36,13 @@ function onColorChange(event) {
     ctx.fillStyle = event.target.value;
 }
 
+function onColorClick(event) {
+    colorValue = event.target.dataset.color;
+    ctx.strokeStyle = colorValue;
+    ctx.fillStyle = colorValue;
+    color.value = colorValue;
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", onMouseDown);
 canvas.addEventListener("mouseup", cancelPainting);
@@ -42,3 +50,5 @@ canvas.addEventListener("mouseleave", cancelPainting);
 
 lineWidth.addEventListener("change", onLineWidthChange);
 color.addEventListener("change", onColorChange);
+
+colorOptions.forEach(color => color.addEventListener("click", onColorClick));
