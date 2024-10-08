f1fd4aa94a525e7257b57964f7cd3ded570396ff
myorange02 <94654972+myorange02@users.noreply.github.com>
2024-10-08 07:15:03 +0000

    #2.0 Painting Lines

--- app.js
+++ app.js
@@ -2,16 +2,38 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
+ctx.lineWidth = 2;
 
-ctx.fillRect(200, 200, 15, 100);
-ctx.fillRect(400, 200, 15, 100);
-ctx.fillRect(260, 200, 60, 200);
+const colors = [
+    "#55efc4",
+    "#81ecec",
+    "#74b9ff",
+    "#a29bfe",
+    "#dfe6e9",
+    "#00b894",
+    "#00cec9",
+    "#0984e3",
+    "#6c5ce7",
+    "#b2bec3",
+    "#ffeaa7",
+    "#fab1a0",
+    "#ff7675",
+    "#fd79a8",
+    "#636e72",
+    "#fdcb6e",
+    "#e17055",
+    "#d63031",
+    "#e84393",
+    "#2d3436"
+]
 
-ctx.arc(290, 150, 50, 0, 2 * Math.PI);
-ctx.fill();
+function onClick(event) {
+    ctx.beginPath();
+    ctx.moveTo(400, 400);
+    const color = colors[Math.floor(Math.random() * colors.length)];
+    ctx.strokeStyle = color;
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+}
 
-ctx.beginPath();
-ctx.fillStyle = "white";
-ctx.arc(270, 150, 10, Math.PI, 2 * Math.PI);
-ctx.arc(300, 150, 10, Math.PI, 2 * Math.PI);
-ctx.fill();
+canvas.addEventListener("mousemove", onClick);
